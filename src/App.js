d3711be5a63d32d45aced12fb1e8be8911cedba7
Bikram-ghuku <bikramghuku05@gmail.com>
2023-07-30 21:49:18 +0530

    added navbar and login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <Routes>
+            <Route exact path='/login' element={<Login/>}/>
+            <Route exact path='/' element={<Home/>}/>
+          </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
