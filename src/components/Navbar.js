392496ffc14331431550f07d081521b1a160e2cf
Bikram Ghuku <bikramghuku05@gmail.com>
2023-12-03 20:54:34 +0530

    fixed navbar alignment issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { Settings } from '@mui/icons-material';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-
+import './css/Navbar.css'
 
 function stringAvatar(name) {
   return {
@@ -55,7 +55,7 @@ function Navbar() {
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav" style={{paddingLeft: "75vw"}}>
+      <ul className="navbar-nav" style={{paddingLeft: "70vw"}}>
         <li className="nav-item">
           <Link className="nav-link" to="/">Home</Link>
         </li>
@@ -74,7 +74,7 @@ function Navbar() {
           </Link>
         </li>
         </> : null}
-        <li>
+        <li className="nav-item">
           <IconButton aria-label="Notifications">
             <Badge badgeContent={0} color="primary">
               <Tooltip title="Notifications"><NotificationsNoneOutlinedIcon/></Tooltip>
@@ -92,27 +92,29 @@ function Navbar() {
         </li>
       </ul>
       <Paper style={{backgroundColor: "#0dcaf0"}}>
-        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClick={handleClose} onClose={handleClose}>
+        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClick={handleClose} onClose={handleClose} >
           <MenuItem>
-            <ListItemIcon>
-              <Settings fontSize='small'/>
-            </ListItemIcon>
-            Settings
+            <Link to="/settings" style={{color:"inherit", textDecoration:"none"}}>
+              <ListItemIcon>
+                <Settings fontSize='small'/>
+              </ListItemIcon>
+              Settings
+            </Link>
           </MenuItem>
           <MenuItem>
-            <a href='/#/logout'>
+            <Link to="/logout" style={{color:"inherit", textDecoration:"none"}}>
               <ListItemIcon>
                 <Logout fontSize="small" />
               </ListItemIcon>
               Logout
-            </a>
+            </Link>
           </MenuItem>
           
         </Menu>
       </Paper>
     </div>
   </nav>
-    )
+  )
 }
 
 export default Navbar;
